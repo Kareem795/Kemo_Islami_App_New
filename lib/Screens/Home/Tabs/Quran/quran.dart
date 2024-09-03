e67e2288a44a53c4a9bde39fea74883f8186c6a3
Kareem795 <kemoyasser2003@gmail.com>
2024-09-03 07:01:10 +0300

    Build quran screen and Create contants class.

---
+++ lib/Screens/Home/Tabs/Quran/quran.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:kemo_islami_app_2/Utils/app_assets.dart';
+import 'package:kemo_islami_app_2/Utils/app_colors.dart';
+import 'package:kemo_islami_app_2/Utils/app_style.dart';
+import 'package:kemo_islami_app_2/Utils/contants.dart';
+
+class Quran extends StatelessWidget 
+{
+  const Quran({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Column
+    (
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: 
+      [
+        Expanded
+        (
+          flex: 30 ,
+          child: Center
+          (
+            child: Image.asset(App_Assets.Quran_Tab_Logo)
+          )
+        ),
+
+        Expanded
+        (
+          flex: 70,
+          child: Stack
+          (
+            alignment: Alignment.center, 
+            //! we use it to make the vertical line in the the center
+
+            children: 
+            [
+              Column
+              (
+                children: 
+                [
+                  build_divider(),
+
+                  const Row
+                  (
+                    children: 
+                    [
+                      Expanded
+                      (
+                        child: Text
+                        (
+                          "Sura Name" ,
+                          textAlign: TextAlign.center,
+                          style: App_Style.title,
+                        )
+                      ),
+
+                      Expanded
+                      (
+                        child: Text
+                        (
+                          "Verses" ,
+                          textAlign: TextAlign.center,
+                          style: App_Style.title,
+                        )
+                      ),
+
+                    ],
+
+                  ),
+
+                  build_divider(),
+
+                  build_suras_list(),
+
+                ],
+              ),
+
+              const VerticalDivider
+              (
+                thickness: 3 , 
+                color: App_Colors.primary , 
+                indent: 9,
+              ) 
+              //! The vertical line to separate the two lists.
+              //! NOTE:  we use (indent: 9) to make the vertical line 
+              //!        not to cross the line
+              //! if you want to try delete (indent: 9)
+
+            ]
+          ),
+        )
+
+      ],
+
+    );
+  }
+
+  Divider build_divider() => const Divider
+  (
+    color: App_Colors.primary, 
+    thickness: 3 , 
+    indent: 10, 
+  );
+
+  /*
+    Container == Divider(color: App_Colors.primary, thickness: 3,),
+    (
+      width: double.infinity,
+      height: 3,
+      color: App_Colors.primary,
+    ),
+        
+  */
+
+  Expanded build_suras_list() => Expanded
+  (
+    flex: 70,
+    child: ListView.builder
+    (
+      itemCount: Contants.suraNames.length, 
+      //! take the number of Sura names and Verses to build the list of Sura names
+
+      itemBuilder: (context , index)
+      //! build the Sura names list and Verses list
+      {
+        return Row
+        (
+          children: 
+          [
+            Expanded  //! اسماء صور
+            (
+              flex: 50,
+              child: Text
+              (
+                Contants.suraNames[index], //! اسم الصورة
+                textAlign: TextAlign.center,
+                style: App_Style.title,
+              )
+            ),
+
+            Expanded //! عدد الايات في كل صورة
+            (
+              flex: 50,
+              child: 
+              Text
+              (
+                Contants.versesNumber[index].toString(), //! عدد اليات
+                textAlign: TextAlign.center,
+                style: App_Style.title,
+              )
+            )
+
+          ],
+
+        );
+      }
+    ),
+  );
+
+}
--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -1,13 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:kemo_islami_app_2/Screens/Home/Tabs/Ahadeth/ahadeth.dart';
+import 'package:kemo_islami_app_2/Screens/Home/Tabs/Quran/quran.dart';
+import 'package:kemo_islami_app_2/Screens/Home/Tabs/Radio/radio.dart';
+import 'package:kemo_islami_app_2/Screens/Home/Tabs/Sebha/sebha.dart';
+import 'package:kemo_islami_app_2/Screens/Home/Tabs/Setting/setting.dart';
+import 'package:kemo_islami_app_2/Utils/app_assets.dart';
+import 'package:kemo_islami_app_2/Utils/app_colors.dart';
 
-class Home extends StatelessWidget 
+class Home extends StatefulWidget 
 {
   static const String route_name = "home";
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> 
+{
+  int selected_tab_index = 0;
+  List<Widget> tabs = [Quran() , Ahadeth() , My_Radio() , Sebha() , Setting()];
+  //! Note: we use List to make move to another tabs more flexible
+
+  @override
   Widget build(BuildContext context) 
   {
-    return const Placeholder();
+    return Container
+    (
+      decoration: const BoxDecoration
+      (
+        image: DecorationImage(image: AssetImage(App_Assets.background))
+      ),
+
+      //! if you want to put a color in container and in the container 
+      //! BoxDecoration you must put the color in the BoxDecoration
+      //! not outside it
+
+      child: Scaffold
+      (
+        backgroundColor: Colors.transparent,
+        appBar: build_Appbar(),
+
+        bottomNavigationBar: build_bottom_Navigation_Bar(),
+
+        body: tabs[selected_tab_index],
+
+
+      ),
+    );
   }
+
+  AppBar build_Appbar() => AppBar
+  (
+    title: Text("Islami"),
+    centerTitle: true,
+    backgroundColor: Colors.transparent,
+    elevation: 0, 
+    //! وظيفة elevathion
+    //! انو بيعملك ظل عشان يحسسك ان AppBar
+    //! مرفوع لفوق 
+    //! و لما خلتوا ب 0 ف انا كدا اخفيت ظل
+
+  );
+        
+  Theme build_bottom_Navigation_Bar() => Theme
+  (
+    //! Note: we use Theme to solve the problem of the navigation bar color
+
+    data: ThemeData(canvasColor: App_Colors.primary),
+    child: BottomNavigationBar
+    (
+      currentIndex: selected_tab_index,
+      //! Note: we use currentIndex to select tab
+
+      onTap: (index) 
+      {
+        selected_tab_index = index;
+        setState(() {}); 
+        //! to update the UI with new selected tab index
+      },
+
+      selectedIconTheme: IconThemeData(size: 36),
+      selectedItemColor: App_Colors.accent,
+
+      items: const
+      [
+        BottomNavigationBarItem(icon: ImageIcon(AssetImage(App_Assets.icon_Quran)) , label: "Quran"),
+        BottomNavigationBarItem(icon: ImageIcon(AssetImage(App_Assets.icon_Ahdath)) , label: "Ahdath"),
+        BottomNavigationBarItem(icon: ImageIcon(AssetImage(App_Assets.icon_Radio)) , label: "Radio"),
+        BottomNavigationBarItem(icon: ImageIcon(AssetImage(App_Assets.icon_Sebha)) , label: "Sebha")
+      ],
+    ),
+  );
 }
